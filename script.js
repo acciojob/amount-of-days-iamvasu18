52c23103795fce9c4d6e671575fc054a6be8ada9
Dharavath Vasu Naik <168115062+iamvasu18@users.noreply.github.com>
2025-09-18 22:20:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+	if(year%400===0){
+		console.log(366);
+	}
+	else if(year%100===0){
+		console.log(365);
+	}
+	else if(year%4===0){
+		console.log(366);
+	}
+}
